1ee733a7b362bae0119020b7b39b509ae54ad79e
hyemch <blossoming33@gmail.com>
2024-11-11 23:30:30 +0900

    feat: exam 01 - 메인에서 메소드 호출

--- java-exam/src/bookstore/BookstoreManagement.java
+++ java-exam/src/bookstore/BookstoreManagement.java
@@ -7,7 +7,6 @@ public class BookstoreManagement {
 	public static final String PINK_LINE = "\u001B[38;5;218m";
 	public static final String BLUE = "\u001B[38;5;39m";
 
-
 	private MyScanner scanner;
 	private Customer customer;
 	private ArrayList<Book> books;
@@ -17,10 +16,10 @@ public class BookstoreManagement {
 		scanner = new MyScanner();
 		books = new ArrayList<>();
 		cart = new Cart();
-		books.add(new Book(1, "셜록홈즈", 20000, "코난도일", "그 누구도 뛰어넘지 못했던 추리 소설의 고전", "추리소설", "2018/10/08" ));
-		books.add(new Book(2, "도리안 그레이의 초상", 16000, "오스카 와일드", "예술을 위한 예술", "고전소설", "2022/01/22" ));
-		books.add(new Book(3, "쥐덫", 27000, "애거서크리스티", "폭설 속에 갇힌 몽스웰 여관 - 네 명의 손님과 주인 부부, 그리고 한 명의 형사", "추리소설", "2019/11/11" ));
-
+		books.add(new Book(1, "셜록홈즈", 20000, "코난도일", "그 누구도 뛰어넘지 못했던 추리 소설의 고전", "추리소설", "2018/10/08"));
+		books.add(new Book(2, "도리안 그레이의 초상", 16000, "오스카 와일드", "예술을 위한 예술", "고전소설", "2022/01/22"));
+		books.add(
+			new Book(3, "쥐덫", 27000, "애거서크리스티", "폭설 속에 갇힌 몽스웰 여관 - 네 명의 손님과 주인 부부, 그리고 한 명의 형사", "추리소설", "2019/11/11"));
 
 		// 장바구니 생성
 		// 책 정보 추가
@@ -52,11 +51,10 @@ public class BookstoreManagement {
 	}
 
 	public void startLibraryManagement() {
-		inputCustomerInfo();
 		while (true) {
 			showDisplay();
-			int selectMenu = scanner.scanInt("메뉴 번호를 선택해주세요: ");
-			switch (selectMenu) {
+			int choice = scanner.scanInt("메뉴 번호를 선택해주세요: ");
+			switch (choice) {
 				case 1 -> {
 					System.out.println(customer.toString());
 				}
@@ -90,6 +88,39 @@ public class BookstoreManagement {
 	public static void main(String[] args) {
 
 		BookstoreManagement bookstoreManagement = new BookstoreManagement();
-		bookstoreManagement.startLibraryManagement();
+		bookstoreManagement.inputCustomerInfo();
+		// bookstoreManagement.startLibraryManagement();
+		while (true) {
+			bookstoreManagement.showDisplay();
+			int choice = bookstoreManagement.scanner.scanInt("메뉴 번호를 선택해주세요: ");
+			switch (choice) {
+				case 1 -> {
+					System.out.println(bookstoreManagement.customer.toString());
+				}
+				case 2 -> {
+					bookstoreManagement.cart.printCart();
+				}
+				case 3 -> {
+					bookstoreManagement.cart.addBook(bookstoreManagement.books);
+				}
+				case 4 -> {
+					bookstoreManagement.cart.removeBook();
+				}
+				case 5 -> {
+					bookstoreManagement.cart.clearCart();
+				}
+				case 6 -> {
+					bookstoreManagement.cart.printReceipt(bookstoreManagement.customer);
+				}
+				case 7 -> {
+					System.out.println("프로그램이 종료되었습니다.");
+					bookstoreManagement.scanner.close();
+					return;
+				}
+				default -> {
+					System.out.println("잘못된 입력입니다. 메뉴 번호를 선택해주세요!");
+				}
+			}
+		}
 	}
 }
