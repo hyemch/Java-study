ba1fa9129fbe8a79d2d845bfceea2888b8546cd1
hyemch <blossoming33@gmail.com>
2024-11-12 18:30:40 +0900

    feat: exam 02 NegativeAmountException 음수 예외

---
+++ java-exam/src/onehanabank/NegativeAmountException.java
@@ -0,0 +1,7 @@
+package onehanabank;
+
+public class NegativeAmountException extends Exception {
+	public NegativeAmountException(String message) {
+		super("Negative Amount is not allowed: " + message);
+	}
+}
