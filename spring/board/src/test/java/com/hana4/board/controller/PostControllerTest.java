ce7e1a415ab5572769203920007ef9d7bf755ade
hyemch <blossoming33@gmail.com>
2024-12-12 10:07:09 +0900

    fix: exam02 spring JPA board - PostControllerTest 수정

--- spring/board/src/test/java/com/hana4/board/controller/PostControllerTest.java
+++ spring/board/src/test/java/com/hana4/board/controller/PostControllerTest.java
@@ -1,6 +1,5 @@
 package com.hana4.board.controller;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -8,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -19,86 +18,79 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hana4.board.dto.PostDTO;
-import com.hana4.board.dto.PostMapper;
-import com.hana4.board.entity.Post;
 import com.hana4.board.entity.User;
 import com.hana4.board.repository.PostRepository;
 import com.hana4.board.repository.UserRepository;
+import com.hana4.board.service.PostService;
 
-import jakarta.transaction.Transactional;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@Transactional
 public class PostControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Autowired
-	private UserRepository userRepository;
-
-	@Autowired
-	private PostRepository postRepository;
-
-	@Autowired
 	private ObjectMapper objectMapper;
 
 	private User user ;
-	private List<PostDTO> samplePosts;
+	private final List<PostDTO> samplePosts = new ArrayList<>();
+	@Autowired
+	private PostService postService;
 
-	@BeforeEach
-	void setUp() {
+	@BeforeAll
+	void setUp(
+		@Autowired PostRepository postRepository, @Autowired UserRepository userRepository) {
 		postRepository.deleteAll();
-		samplePosts = new ArrayList<>();
 		user = userRepository.findAll().get(0);
-		for (int i = 1; i <= 3; i++) {
-			PostDTO postDTO = new PostDTO("Post title" + i, user.getId(), "Post body" + i);
-			Post savedPost = postRepository.save(PostMapper.toEntity(postDTO, user));
-			postDTO.setId(savedPost.getId());
-			samplePosts.add(postDTO);
+
+		for (int i = 0; i < 2; i++) {
+			PostDTO dto = new PostDTO();
+			dto.setWriter(user.getId());
+			dto.setTitle("Post" + i);
+			dto.setBody("PostBody" + i);
+			samplePosts.add(dto);
 		}
 	}
 
 	@Test
 	@Order(1)
-	void testGetAllPosts() throws Exception {
-		System.out.println("samplePosts size= " + samplePosts.size());
-		mockMvc.perform(get("/posts"))
-			.andExpect(status().isOk())
-			.andExpect(jsonPath("$.length()").value(samplePosts.size()))
-			.andExpect(jsonPath("$[0].id").value(samplePosts.get(0).getId()))
-			.andDo(print());
-	}
-
-	@Test
-	@Order(2)
 	void testCreatePost() throws Exception {
-		for (int i = 1; i <= 3; i++) {
-			PostDTO postDTO = new PostDTO("Create Post" + i, user.getId(),
-				"Post Body" + i);
+		for(PostDTO postDTO : samplePosts) {
 			String requestBody = objectMapper.writeValueAsString(postDTO);
-			mockMvc.perform(post("/posts")
+			final ResultActions result = mockMvc.perform(post("/posts")
 					.contentType(MediaType.APPLICATION_JSON)
-					.content(requestBody))
-				.andExpect(status().isCreated())
+					.content(requestBody));
+
+				result.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.title").value(postDTO.getTitle()))
 				.andExpect(jsonPath("$.body").value(postDTO.getBody()))
 				.andExpect(jsonPath("$.writer").value(user.getId()))
 				.andDo(print());
-			samplePosts.add(postDTO);
 		}
+	}
+
+	@Test
+	@Order(2)
+	void testGetAllPosts() throws Exception {
+		mockMvc.perform(get("/posts"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.length()").value(samplePosts.size()))
+			.andExpect(jsonPath("$[0].title").value(samplePosts.get(0).getTitle()))
+			.andDo(print());
 	}
 
 	@Test
 	@Order(3)
 	void testGetPostById() throws Exception {
-
-		Long postId = samplePosts.get(0).getId();
+		List<PostDTO> posts = postService.getAllPosts();
+		Long postId = posts.get(0).getId();
 		mockMvc.perform(get("/posts/" + postId))
 			.andExpect(status().isOk())
 			.andExpect(jsonPath("$.id").value(postId))
@@ -110,9 +102,10 @@ public class PostControllerTest {
 	@Test
 	@Order(4)
 	void testUpdatePost() throws Exception {
-		Long postId = samplePosts.get(0).getId();
-		PostDTO updatePost = new PostDTO("Updated Title", user.getId(), "Updated Body");
-
+		PostDTO updatePost = postService.getAllPosts().get(0);
+		Long postId = updatePost.getId();
+		updatePost.setTitle("Updated Title");
+		updatePost.setBody("Updated Body");
 		String requestBody = objectMapper.writeValueAsString(updatePost);
 
 		mockMvc.perform(patch("/posts/" + postId)
@@ -122,18 +115,16 @@ public class PostControllerTest {
 			.andExpect(jsonPath("$.title").value("Updated Title"))
 			.andExpect(jsonPath("$.body").value("Updated Body"))
 			.andDo(print());
-		assertThat(postRepository.findById(postId)).get().hasFieldOrPropertyWithValue("title", updatePost.getTitle());
 	}
 
 	@Test
 	@Order(5)
 	void testDeletePost() throws Exception {
-		Long postId = samplePosts.get(1).getId();
+		PostDTO deletePost = postService.getAllPosts().get(0);
+		Long postId = deletePost.getId();
 
 		mockMvc.perform(delete("/posts/" + postId))
 			.andExpect(status().isNoContent())
 			.andDo(print());
-
-		assertThat(postRepository.findById(postId)).isEmpty();
 	}
 }
